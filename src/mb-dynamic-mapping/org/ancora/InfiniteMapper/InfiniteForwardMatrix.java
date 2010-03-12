aa5f7686c9bdeb280eeedb06fb19b679b477467d
joaobispo <joaobispo@62d7095c-e67b-11de-b92f-5d3588262692>
2010-03-12 18:46:00 +0000

    [Mb-Dynamic-Mapping] - Adapted InfiniteForwardMatrix mapping to IR;

---
+++ src/mb-dynamic-mapping/org/ancora/InfiniteMapper/InfiniteForwardMatrix.java
@@ -0,0 +1,200 @@
+/*
+ *  Copyright 2010 Ancora Research Group.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+
+package org.ancora.InfiniteMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+import org.ancora.IrForDynamicMapping.Operand;
+import org.ancora.IrForDynamicMapping.Operation;
+
+/**
+ * Repreents an Infinite Forward Matrix which accepts maps of parameterizable
+ * elements.
+ *
+ * @author Joao Bispo
+ */
+public class InfiniteForwardMatrix {
+
+   public InfiniteForwardMatrix() {
+      lines = new ArrayList<IfmMapperLine>();
+   }
+
+   public int getFirstAvaliableColumn(int line) {
+      // Check if line exists. If not, create the lines
+      boolean lineExists = lineExists(line);
+      if(!lineExists) {
+         createLines(line);
+      }
+      // Get line size
+      return lines.get(line).getSize();
+   }
+
+   /**
+    * There are no outputs because at map time... the output is the self?
+    * @param line
+    * @param col
+    * @param operation
+    * @param inputs
+    * @return
+    */
+   public Fu mapNewOperation(int line, int col, Operation operation,  Operand[] inputs) {
+      // Create Operand for Fu Coordinate
+      Operand fuSelfCoordinate = IfmUtils.newCoordinateOperand(line, col);
+      // Create Fu
+      Fu fu = new Fu(operation, inputs, fuSelfCoordinate);
+
+      //Add Fu to the matrix
+      boolean success = addFu(line, col, fu);
+      if(!success) {
+         Logger.getLogger(InfiniteForwardMatrix.class.getName()).
+                 warning("Could not add FU "+fu+" to matrix.");
+      }
+
+      return fu;
+   }
+
+   public Fu mapMoveOperation(int line, int col, Operand input) {
+      // Create Operand for Fu Coordinate
+      Operand fuSelfCoordinate = IfmUtils.newCoordinateOperand(line, col);
+      // Create Fu
+      Operand[] inputs = new Operand[1];
+      inputs[0] = input;
+      Fu fu = new Fu(null, inputs, fuSelfCoordinate);
+
+      //Add Fu to the matrix
+      boolean success = addFu(line, col, fu);
+      if(!success) {
+         Logger.getLogger(InfiniteForwardMatrix.class.getName()).
+                 warning("Could not add FU "+fu+" to matrix.");
+      }
+
+      return fu;
+   }
+
+
+   private boolean lineExists(int line) {
+      return line < lines.size();
+   }
+
+   private void createLines(int line) {
+      /*
+       * Recursive Version
+      if(lineExists(line)) {
+         return;
+      }
+
+      createLines(line-1);
+      lines.add(new IfmMapperLine<T>());
+       */
+      // How many lines need to be created
+      int numberOfLines = line-lines.size()+1;
+
+      for(int i=0; i<numberOfLines; i++) {
+         lines.add(new IfmMapperLine());
+      }
+   }
+
+
+   private boolean addFu(int line, int col, Fu fu) {
+      // Get line
+      IfmMapperLine mapperLine = null;
+      try {
+         mapperLine = lines.get(line);
+      } catch(IndexOutOfBoundsException ex) {
+         Logger.getLogger(InfiniteForwardMatrix.class.getName()).
+                 warning("Line index '"+line+"' does not exist. Mapper has '"+
+                 lines.size()+"' lines.");
+         return false;
+      }
+
+      // Add to column
+      // Check if position already has Fu
+      if(col < mapperLine.getSize()) {
+         mapperLine.setFu(col, fu);
+         Logger.getLogger(IfmMapperLine.class.getName()).
+                 warning("Replacing Fu at line '"+line+"', column '"+col+"'");
+         return true;
+      }
+      // Check if position is the next Fu
+      if(col == mapperLine.getSize()) {
+         mapperLine.addFu(fu);
+         return true;
+      }
+
+      Logger.getLogger(IfmMapperLine.class.getName()).
+              warning("Could not map Fu at line '" + line + "', column '" + col + "': line has '"+
+              mapperLine.getSize()+"' FUs.");
+      return false;
+   }
+
+
+   public int getNumberOfMappedLines() {
+      return lines.size();
+   }
+
+   public int getMoveOperations() {
+      int totalOps = 0;
+      for(IfmMapperLine line : lines) {
+         for(int i=0; i<line.getSize(); i++) {
+            if(line.getFu(i).getOperation() == null) {
+               totalOps++;
+            }
+         }
+      }
+      return totalOps;
+   }
+
+/**
+ * Total Operatiosn, counting move operations
+ * @return
+ */
+   public int getNumberOfMappedOps() {
+      int totalOps = 0;
+      for(IfmMapperLine line : lines) {
+         totalOps += line.getSize();
+      }
+      return totalOps;
+   }
+
+   public float getIlp() {
+      if(getNumberOfMappedLines() == 0) {
+         return 0;
+      }
+
+      int mappedOperationsWithoutMoves = getNumberOfMappedOps()-getMoveOperations();
+      return (float)mappedOperationsWithoutMoves / (float)getNumberOfMappedLines();
+   }
+
+   public float getIlpWithMoves() {
+      if(getNumberOfMappedLines() == 0) {
+         return 0;
+      }
+
+      return (float)getNumberOfMappedOps() / (float)getNumberOfMappedLines();
+   }
+
+
+   /**
+    * INSTANCE VARIABLES
+    */
+   private List<IfmMapperLine> lines;
+
+
+
+}
